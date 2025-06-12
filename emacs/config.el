4ae07de1d1415a757d07540ca918a3dcedec29f6
James Patrick <james@greenfiling.com>
2025-06-12 13:36:49 -0400

    Special Keys require []
    See https://www.gnu.org/software/emacs/manual/html_node/use-package/Global-keybindings.html#Using-special-keys

--- emacs/config.el
+++ emacs/config.el
@@ -47,7 +47,5 @@
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+              ("[tab]" . 'copilot-accept-completion)
+              ("[C-TAB]" . 'copilot-accept-completion-by-word)))
