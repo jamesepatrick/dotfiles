2952367f5fedc5fd9c4841fe4ad6efdf87627cb8
James Patrick <james@greenfiling.com>
2025-06-15 10:46:07 -0400

    Added Keybinds for Copliot Chat

--- emacs/+keybinds.el
+++ emacs/+keybinds.el
@@ -28,6 +28,42 @@
       )
 
 (map! :leader
+      (:prefix "o"
+       :desc "Copilot Chat" "c" #'copilot-chat
+       )
+      )
+
+(map! :leader
+      (:prefix-map ("m" . "Copilot Magic")
+       :desc "Transient"          "k"    #'copilot-chat-transient
+       :desc "Add Current Buffer" "a"    #'copilot-chat-add-current-buffer
+       :desc "Go To"              "g"    #'copilot-chat-switch-to-buffer
+       :desc "Hide"               "x"    #'copilot-chat-hide
+       :desc "Ask & Insert"       "i"    #'copilot-chat-ask-and-insert
+       :desc "Select Model"       "m"    #'copilot-chat-set-model
+       :desc "Quick Prompt"       "q"    #'copilot-chat-custom-prompt-mini-buffer
+       :desc "Reset"              "r"    #'copilot-chat-reset
+       :desc "Quick Prompt"       "q"    #'copilot-chat-custom-prompt-mini-buffer
+       :desc "Quick Prompt"       "q"    #'copilot-chat-custom-prompt-mini-buffer
+       (:prefix-map ("c" . "Context")
+          :desc "Add Current Buffer" "a" #'copilot-chat-add-current-buffer
+          :desc "Add File"           "f" #'copilot-chat-add-file
+          :desc "Remove Buffer"      "r" #'copilot-chat-del-buffers
+          :desc "List Files"         "l" #'copilot-chat-list
+          :desc "Add Workspace"      "w" #'copilot-chat-add-workspace
+          :desc "Add Directory"      "d" #'copilot-chat-add-files-under-dir
+          :desc "Transient Code"     "c" #'copilot-chat-transient-code)
+       (:prefix-map ("h" . "Helper")
+          :desc "Explain"             "e" #'copilot-chat-explain
+          :desc "Documentation"       "d" #'copilot-chat-doc
+          :desc "Fix"                 "f" #'copilot-chat-fix
+          :desc "Optimize"            "o" #'copilot-chat-optimize
+          :desc "Test"                "t" #'copilot-chat-test
+          :desc "Custom Prompt"       "c" #'copilot-chat-custom-prompt-function
+       )
+      )
+     )
+(map! :leader
       (:prefix-map ("A" . "GPT")
        :desc "Menu"              "m"    #'gptel-menu
        :desc "Menu"              "a"    #'gptel-menu
